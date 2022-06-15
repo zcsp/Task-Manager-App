841255e24479e4b429f227f635af43618e87d0fd
spark33 <seanpark1107@gmail.com>
2022-06-15 16:03:16 -0400

    router basename

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ProjectPage from './pages/ProjectPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/project/:project_id" element={<ProjectPage />} />
