d1ff985dc645fe05ba325dc1153a2878926f2d43
spark33 <seanpark1107@gmail.com>
2022-06-15 16:16:18 -0400

    heroku

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://radiant-wildwood-39217.herokuapp.com';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
